3fd9491087518aca935032c49891c4d7b4bac70d
alejandro.velez <labvel@VelPro.>
2025-02-13 12:50:43 -0500

    fix: overwrite outputs with sensitive values

--- infrastructure/containers/gitops_bridge/outputs.tf
+++ infrastructure/containers/gitops_bridge/outputs.tf
@@ -1,10 +1,12 @@
 output "argocd" {
   description = "Argocd helm release"
   value       = try(helm_release.argocd[0], null)
+  sensitive = true
 }
 output "cluster" {
   description = "ArgoCD cluster"
   value       = try(kubernetes_secret_v1.cluster[0], null)
+  sensitive = true
 }
 output "apps" {
   description = "ArgoCD apps"
